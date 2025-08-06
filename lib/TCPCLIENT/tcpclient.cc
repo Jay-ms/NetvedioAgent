c8f43d78225f517457d83eece8c8d7683b9fc986
Jay-ms <1518504125@qq.com>
2025-08-06 21:25:55 +0800

    add servo anglespeed control

--- lib/TCPCLIENT/tcpclient.cc
+++ lib/TCPCLIENT/tcpclient.cc
@@ -32,6 +32,7 @@ void tcpclient_run(int connfd, std::atomic<bool>& servo_mode, std::atomic<int>& 
 {
     char buff[MAX];
     printf("TCP client is running...\n");
+    int angleSpeed = 5; // Default angle speed
     while(1)
     {
         bzero(buff, MAX);
@@ -43,8 +44,19 @@ void tcpclient_run(int connfd, std::atomic<bool>& servo_mode, std::atomic<int>& 
         }
         // 打印接收到的数据
         buff[n] = '\0'; // 确保字符串以null结尾
+        
+        // 先移除换行符
         buff[strcspn(buff, "\r\n")] = '\0';
         printf("From Server: %s\n", buff);
+        
+        // 提取最后一个字符作为速度值并转换为角度步进
+        int len = strlen(buff);
+        char speedChar = buff[len - 1];
+        if (speedChar >= '1' && speedChar <= '9') {
+            angleSpeed = speedChar - '0';
+            printf("angle speed: %d\n", angleSpeed);
+        } 
+
 
         if(strcmp("manual", buff) == 0)
         {
@@ -59,19 +71,19 @@ void tcpclient_run(int connfd, std::atomic<bool>& servo_mode, std::atomic<int>& 
         {
             switch (buff[0]) {
                 case 'l': // Increase yaw angle
-                    yaw_servo_angle += 5;
+                    yaw_servo_angle += angleSpeed;
                     printf("Yaw angle increased to: %d\n", yaw_servo_angle.load());
                     break;
                 case 'r': // Decrease yaw angle
-                    yaw_servo_angle -= 5;
+                    yaw_servo_angle -= angleSpeed;
                     printf("Yaw angle decreased to: %d\n", yaw_servo_angle.load());
                     break;
                 case 'u': // Increase pitch angle
-                    pitch_servo_angle += 5;
+                    pitch_servo_angle += angleSpeed;
                     printf("Pitch angle increased to: %d\n", pitch_servo_angle.load());
                     break;
                 case 'd': // Decrease pitch angle
-                    pitch_servo_angle -= 5;
+                    pitch_servo_angle -= angleSpeed;
                     printf("Pitch angle decreased to: %d\n", pitch_servo_angle.load());
                     break;
                 case 'R': // Decrease pitch angle
